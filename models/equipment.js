4ab9f8228759ae48ed472b6bcaeba7c041589b4d
Dipesh <dipeshchourasiya12@gmail.com>
2025-03-10 16:19:19 +0530

    ad

--- models/equipment.js
+++ models/equipment.js
@@ -14,7 +14,6 @@ const equipmentSchema = new mongoose.Schema(
     },
     condition: {
       type: String,
-      required: true,
     },
     location: {
       type: String,
--- routes/equipment.js
+++ routes/equipment.js
@@ -32,7 +32,7 @@ router.post("/add-equipment", authenticateToken, async (req, res) => {
 
     return res.status(200).json({ message: "Equipment added successfully." });
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error", error:error });
   }
 });
 
